11c01346c1b7ec4574e3dda2ac2d8e81481e814b
Asus <dhanashreehakke7@gmail.com>
2021-11-03 14:33:44 +0530

    Added: Welcome message is added

---
+++ src/com/blp/objectorientedprogramming/StockAccountManagement.java
@@ -0,0 +1,11 @@
+package com.blp.objectorientedprogramming;
+
+/**
+ * Stock Account Management
+ */
+public class StockAccountManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to object oriented programming");
+    }
+}
